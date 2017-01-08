01fbc85fded9a460eaec0d770b60f4734b93438c
yudawu <yudawu@DESKTOP-CBA8THA>
2017-01-08 13:42:21 +0800

    實作客戶資料與訂單相關 APIs /clients /clients/1 /clients/1/orders /clients/1/orders/2017-01-08 /clients/1/orders/top10

---
+++ WebApplication1/Controllers/ClientsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApi170108.Controllers
+{
+    [RoutePrefix("clients")]
+    public class ClientsController : ApiController
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        public ClientsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        [Route("")]
+        public IQueryable<Client> GetClient()
+        {
+            return db.Client;
+        }
+
+        [Route("{id:int}")]
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult GetClient(int id)
+        {
+            Client client = db.Client.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        [Route("{id:int}/orders")]
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetClientOrders(int id)
+        {
+            var orders = db.Order.Where(p => p.ClientId == id);
+
+            return Ok(orders.ToList());
+        }
+
+        [Route("{id:int}/orders/{*date:datetime}")]
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetClientOrdersByDate(int id, DateTime date)
+        {
+            var date_begin = date.Date;
+            var date_end = date.AddDays(1);
+
+            var orders = db.Order.Where(p =>
+            p.ClientId == id &&
+            (
+            p.OrderDate.Value > date_begin &&
+            p.OrderDate.Value < date_end
+            ));
+
+            return Ok(orders.ToList());
+        }
+
+        [Route("{id:int}/orders/top10")]
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetClientOrdersByTop10(int id)
+        {
+            var orders = db.Order
+            .Where(p => p.ClientId == id)
+            .OrderByDescending(p => p.OrderDate)
+            .Take(10);
+
+            return Ok(orders.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return db.Client.Count(e => e.ClientId == id) > 0;
+        }
+    }
+}
